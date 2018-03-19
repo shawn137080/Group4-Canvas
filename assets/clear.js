b3ee8db135044fa04ad15ce6fe48ea0afd1f29a1
judith curtit <judithcurtit8@gmail.com>
2018-03-19 22:39:30 +0800

    to be reviewed

--- assets/clear.js
+++ assets/clear.js
@@ -1,4 +1,7 @@
-function clearCanvas()
-{
-	contextReal.clearRect(0, 0, canvasWidth, canvasHeight);
+function redraw(){
+	context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 }
+// $("#clear").click(function clearCanvas(contextReal, contextDraft){
+// 	contextReal.clearRect(0, 0, 800, 400);
+// 	contextDraft.clearRect(0, 0, 800, 400);
+// });
--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,23 @@
+$(window).on('load', function(){
 
+    let canvasCoverage = 0.8;
+    let buttonboxWidth = $('#button-box').width();
+    let pickerboxHeight = $('#picker-box').height();
+    
+    //Set the size for the canvas
+    let canvas_width = ($(window).width()-buttonboxWidth)*canvasCoverage;
+    let canvas_width_margin = ($(window).width()-buttonboxWidth)*(1-canvasCoverage)/2;
+    let canvas_height_margin = ($(window).height()-pickerboxHeight)*(1-canvasCoverage)/2;
+    let canvas_height = ($(window).height()-pickerboxHeight)*canvasCoverage;
+    $( "#canvas-real" )[0].width = $( "#canvas-draft" )[0].width = canvas_width;
+    $( "#canvas-real" )[0].height = $( "#canvas-draft" )[0].height = canvas_height;
+    $( "#canvas-real" ).css({marginLeft: `${canvas_width_margin  + buttonboxWidth}px`, 
+                             marginTop: `${canvas_height_margin + pickerboxHeight}px`});
+    $( "#canvas-draft" ).css({marginLeft: `${canvas_width_margin  + buttonboxWidth}px`, 
+                              marginTop: `${canvas_height_margin + pickerboxHeight}px`});
+});
+
+    $( document ).ready(function() {
     $('#drawing-rectangle').click(()=>{
     currentFunction = new DrawingRectangle(contextReal,contextDraft);
     });
@@ -26,7 +45,10 @@
     $('#color-label').dblclick(function(){
         $('#color-picker').hide()
     });
+    $('#clear').click(function redraw(){
+    });
     currentFunction = new DrawingRectangle(contextReal,contextDraft);
+    });
     // $('#upload-img').click(()=>{
     // currentFunction = new FileReader (contextReal,contextDraft);
     // });
